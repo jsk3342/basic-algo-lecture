02fb493bd63bd22627434d526082b5959e4f5515
jsk3342 <kss2460@naver.com>
2022-08-08 19:49:01 +0900

    Add: 자릿수 곱셈 문제 해결

---
+++ 0x00/b10430/app.js
@@ -0,0 +1,13 @@
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+
+let input = fs.readFileSync(filePath).toString().split("\n");
+input = input[0].split(" ").map(i => +i);
+
+var A = input[0];
+var B = input[1];
+var C = input[2];
+console.log((A + B) % C);
+console.log(((A % C) + (B % C)) % C);
+console.log((A * B) % C);
+console.log(((A % C) * (B % C)) % C);
---
+++ 0x00/b2588/app.js
@@ -0,0 +1,36 @@
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+
+let input = fs.readFileSync(filePath).toString().split("\n");
+input = input.map(Number).map(i => i.toString());
+var arrA = input[0].split("");
+var arrB = input[1].split("");
+
+var 자리수쪼개기 = arrA.map((item, index) => {
+  if (index === 0) {
+    return item * 100;
+  } else if (index === 1) {
+    return item * 10;
+  } else if (index === 2) {
+    return +item;
+  }
+});
+
+var 삼번더하기 = 0;
+var 사번더하기 = 0;
+var 오번더하기 = 0;
+자리수쪼개기.forEach(i => {
+  삼번더하기 = 삼번더하기 + i * arrB[2];
+});
+자리수쪼개기.forEach(i => {
+  사번더하기 = 사번더하기 + i * arrB[1];
+});
+자리수쪼개기.forEach(i => {
+  오번더하기 = 오번더하기 + i * arrB[0];
+});
+
+console.log(삼번더하기);
+console.log(사번더하기);
+console.log(오번더하기);
+console.log(삼번더하기+사번더하기*10+오번더하기*100);
+
