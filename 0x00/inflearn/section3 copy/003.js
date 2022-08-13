9bd954a60f2c3aba8bcc6ac734a0063874f9f7bc
jsk3342 <kss2460@naver.com>
2022-08-13 21:05:28 +0900

    Add: 섹션3-3 문제 해결

--- 0x00/inflearn/section3 copy/003.js
+++ 0x00/inflearn/section3 copy/003.js
@@ -1,26 +1,12 @@
- function solution(s, t){
-                let answer=[];
-                let p=1000;
-                for(let x of s){
-                    if(x===t){
-                        p=0;
-                        answer.push(p);
-                    }
-                    else{
-                        p++;
-                        answer.push(p);
-                    }
-                }
-                p=1000;
-                for(let i=s.length-1; i>=0; i--){
-                    if(s[i]===t) p=0;
-                    else{
-                        p++;
-                        answer[i]=Math.min(answer[i], p);
-                    }
-                }
-                return answer;
-            }
-            
-            let str="teachermode";
-            console.log(solution(str, 'e'));
+function solution(str) {
+  answer = "";
+  for (let x of str) {
+    if (!isNaN(x)) {
+      answer += x;
+    }
+  }
+  console.log(+answer);
+}
+
+let str = "g0en2T0s8eSoft";
+console.log(solution(str));
