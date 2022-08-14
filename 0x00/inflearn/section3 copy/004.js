d17d249a810a4b1a1ccf61d168466c1d8b863987
jsk3342 <kss2460@naver.com>
2022-08-14 21:36:13 +0900

    Add: 섹션3 문제 해결

--- 0x00/inflearn/section3 copy/004.js
+++ 0x00/inflearn/section3 copy/004.js
@@ -1,47 +1,26 @@
-// function solution(s, t) {
-//   let answer = [];
-//   let arr = new Array(s.length).fill(0);
-//   let 찾는값 = str.indexOf("e");
-
-//   for (let i = 0; i < s.length; i++) {
-//     if (s[i]);
-//   }
-
-//   return answer;
-// }
-
-// let str = "teachermode";
-// console.log(solution(str, "e"));
-
-// 풀이 방법
-// 1. 기준 단어를 찾는다
-// 2. 인덱스오브를 이용하여 인덱스 위치를 찾는다
-// 3. 만약 기준단어 왼쪽에 있으면 기준단어 인덱스 - 현위치 단어
-// 4. 오른쪽ㅇ이면 현단어 위치 - 기준단어
-// 5. 서로 값 비교해서 적은 값으로 대체 한다
-
-// 풀이 방법
-// 1. 좌우로 반복문 돈다
-
-function solution(s, t) {
+function solution(arr, t) {
   let answer = [];
+  let arr1 = arr.split("");
   let p = 1000;
-  for (let i = 0; i < s.length; i++) {
-    if (s[i] !== t) {
+  for (let i = 0; i < arr.length; i++) {
+    if (t !== arr1[i]) {
       p++;
+      answer.push(p);
     } else {
       p = 0;
+      answer.push(p);
     }
-    answer.push(p);
   }
 
-  for (let i = s.length; i >= 0; i--) {
-    if (s[i] !== t) {
+  for (let i = arr.length; i >= 0; i--) {
+    if (t !== arr1[i]) {
       p++;
     } else {
       p = 0;
     }
-    if (p < answer[i]) answer[i] = p;
+    if (answer[i] > p) {
+      answer[i] = p;
+    }
   }
 
   return answer;
--- 0x00/inflearn/section3 copy/005.js
+++ 0x00/inflearn/section3 copy/005.js
@@ -2,12 +2,14 @@ function solution(s) {
   let answer = "";
   let count = 1;
   for (let i = 0; i < s.length; i++) {
-    if (s[i] !== s[i + 1]) {
+    if (s[i] == s[i + 1]) {
+      count++;
+    } else {
       if (count !== 1) {
         answer += s[i] + count;
         count = 1;
       } else answer += s[i];
-    } else count++;
+    }
   }
   return answer;
 }
