1560cf3fa1d72985d7635b1e23e10a3cb5b84ec1
jisu-kim <kss2460@naver.com>
2022-09-04 17:59:46 +0900

    Add: section7 try

--- 0x00/inflearn/section7/011.js
+++ 0x00/inflearn/section7/011.js
@@ -1,8 +1,11 @@
-function solution(m, songs){
+function solution(m, songs) {
   let answer;
 
   return answer;
 }
 
-let arr=[1, 2, 3, 4, 5, 6, 7, 8, 9];
+let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 console.log(solution(3, arr));
+
+//최소 크기는 9가 돼야 함 9장으로
+// 만약 3장으로 하고 싶다면?
