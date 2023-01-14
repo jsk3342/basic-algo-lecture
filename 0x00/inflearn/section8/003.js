5da372cb175543384036e7e1ab0700f8715ab02a
JiSu Kim <85912592+jsk3342@users.noreply.github.com>
2023-01-14 20:04:40 +0900

    add: DFS

--- 0x00/inflearn/section8/003.js
+++ 0x00/inflearn/section8/003.js
@@ -1,10 +1,16 @@
-function solution(n){
-  let answer=[];
-  function DFS(n) {
-
+function solution(v) {
+  let answer;
+  function DFS(v) {
+    if (v > 7) {
+      return;
+    } else {
+      console.log(v);
+      DFS(v * 2);
+      DFS(v * 2 + 1);
+    }
   }
-  DFS(1);
+  DFS(v);
   return answer;
 }
 
-console.log(solution(3));
+solution(3);
