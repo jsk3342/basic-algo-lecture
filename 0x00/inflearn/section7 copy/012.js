bc404a86e1edd21bd93c61a52a48405c513fbc41
JiSu Kim <85912592+jsk3342@users.noreply.github.com>
2023-02-08 22:09:58 +0900

    add: section7

---
+++ 0x00/inflearn/section7 copy/012.js
@@ -0,0 +1,14 @@
+function count(stable, dist) {
+  let cnt = 1,
+    ep = stable[0];
+
+  return cnt;
+}
+function solution(c, stable) {
+  let answer;
+
+  return answer;
+}
+
+let arr = [1, 2, 8, 4, 9];
+console.log(solution(3, arr));
