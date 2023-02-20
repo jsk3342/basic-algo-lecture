3d906cd7785a04fad77fb6dc8de5a827d32181fd
jisu-kim <kss2460@naver.com>
2023-02-20 20:32:55 +0900

    add: 수학문제 추가

---
+++ 0algo/math/b10430/app.js
@@ -0,0 +1,10 @@
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+let input = fs
+  .readFileSync(filePath)
+  .toString()
+  .split(" ")
+  .map(i => +i);
+
+console.log("시작 인풋", input);
+
