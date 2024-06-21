10494d87b4551f94a991caa186df794dc5083d3a
jisu-kim <kss2460@naver.com>
2024-06-21 16:21:21 +0900

    section4 풀이 브루투 포스

--- 0JS/section4/005.js
+++ 0JS/section4/005.js
@@ -1,4 +1,15 @@
-function solution(n, k, card) {}
+function solution(n, k, card) {
+  const arr = [];
+  for (let i = 0; i < card.length - 2; i++) {
+    for (let j = i + 1; j < card.length - 1; j++) {
+      for (let k = j + 1; k < card.length; k++) {
+        arr.push(card[i] + card[j] + card[k]);
+      }
+    }
+  }
+  arr.sort((a, b) => b - a);
+  return arr[k - 1];
+}
 
 let arr = [13, 15, 34, 23, 45, 65, 33, 11, 26, 42];
 console.log(solution(10, 3, arr));
