9e7422aa24c1cb7f483ae169c4a73e56038da769
jisu-kim <kss2460@naver.com>
2024-06-26 15:27:14 +0900

    section7 풀이

--- 0JS/section7/005.js
+++ 0JS/section7/005.js
@@ -1,5 +1,15 @@
-function solution(arr) {
-  
+function solution(size, arr) {
+  const memory = Array(size).fill(0);
+  for (let num of arr) {
+    memory.unshift(num);
+    if (memory.lastIndexOf(num) != 0) {
+      memory.splice(memory.lastIndexOf(num), 1);
+    } else {
+      memory.pop();
+    }
+    console.log(memory);
+  }
+  return memory;
 }
 
 let arr = [1, 2, 3, 2, 6, 2, 3, 5, 7];
