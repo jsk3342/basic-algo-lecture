b505b5946869c6f5bb5150f565913c3033faddb6
jisu-kim <kss2460@naver.com>
2024-07-12 15:49:50 +0900

    section4 다른 풀이

---
+++ 0JS/section4/001-1.js
@@ -0,0 +1,20 @@
+function solution(arr) {
+  let answer = 0;
+  let maxSum = 0;
+  for (let num of arr) {
+    const sum = num
+      .toString()
+      .split("")
+      .map(Number)
+      .reduce((a, b) => a + b);
+    maxSum = Math.max(sum, maxSum);
+    if (maxSum == sum) {
+      answer = Math.max(answer, num);
+    }
+  }
+
+  return answer;
+}
+
+let arr = [128, 460, 603, 40, 521, 137, 123];
+console.log(solution(arr));
